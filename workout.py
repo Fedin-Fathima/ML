1d30296db888a14660d695db91474697fd6b462f
Fedin-Fathima <fedifathima2004@gmail.com>
2025-03-17 15:16:03 +0530

    Add files via upload

---
+++ workout.py
@@ -0,0 +1,138 @@
+
+
+# a=[1,2,3,4,5]
+# print(a)
+# a.clear()
+# print(a)
+
+# a=[5,8,9,55,2,1,9]
+# a.sort()
+# print(a)
+
+# a={"name":"fedin","age":20,"education":"under graduate"}
+# b={"name":"neenu","age":11,"education":"school going"}
+
+# a.clear()
+# print(a)
+
+# a.copy()
+# print(a)
+
+# a=('key 1','key 2','key 3')
+# b=0
+# x=dict.fromkeys(a,b)
+# print(x)
+
+# c=a.get("age")
+# print(c)
+
+# c=a.items()
+# print(c)
+
+# x=a.keys()
+# print(x)
+
+# a.popitem()
+# print(a)
+
+# a.pop("age")
+# print(a)
+
+# x=a.setdefault("name")
+# print(x)
+
+# a.update({"place":"poonoor"})
+# print(a)
+
+# x=a.values()
+# print(x)
+
+
+
+# a=[1,2,3,4,5,6,7,8,9,10]
+
+# a.append(11)
+# print(a)
+
+# a.clear()
+# print(a)
+
+# a.copy()
+# print(a)
+
+# print(a.count(2))
+
+# a.sort()
+# print(a)
+
+# a.insert(0,-1)
+
+# print(a)
+
+# a=int(input("Enter a number"))
+# if (a%2==0):
+#     print("the number is a multiple")
+# else:
+#     print("not a multiple")    
+
+
+# a=[1,2,3,4,5,6]
+# print(a[1])
+# print(a[-2])
+
+# print(a[2:4])
+
+# a=5
+# b=8
+# c=a
+# a=b
+# b=c
+# print(a)
+# print(b)
+
+
+# a=[1,2,3,4,5]
+# b=set(a)
+# print(b)
+
+# a=set()
+# print(type(a))
+# print(a)
+
+
+
+# a="hello world"
+# print(a[:-6:-1])
+
+# a={''}
+# print(type(a))
+
+# a={1,2,3,4,5}
+# b=list(a)
+# print(a)
+
+# a=10
+# b=3
+# a,b=b,a
+# print(a)
+# print(b)
+
+# a=10
+# b=4
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+# number1=float(input("Enter first number :"))
+# number2=float(input("Enter second number"))
+
+# a={"fedin":3456,"risha":4576,"hiba":5678}
+# b=str(input("enter a string :"))
+# c=int(input("enter the number :"))
+# a[b]=c
+# print(a)
+
+
+
